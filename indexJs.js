2d4dafa3cb70d3c2e36d54a412813bb207d0bcf6
soheilhosseini <hosseini.soheil@yahoo.com>
2017-12-20 00:55:24 +0330

    ezafe kardane akse + skills

--- indexJs.js
+++ indexJs.js
@@ -1,7 +1,21 @@
-
-
-
 // var height = $(window).height();
 // $('#mainLeftDiv').css('height', height);
 // $('#mainCenterDiv').css('height', height);
 // $('#mainRightDiv').css('height', height);
+
+
+// setTimeout(skillsPhotoBlinkFadeIn,1000);
+// setTimeout(skillsPhotoBlinkFadeOut,2500);
+//
+// function skillsPhotoBlinkFadeIn() {
+//     $('.SkillsPhoto ').css('transition','1s');
+//     $('.SkillsPhoto img:first-child').css('opacity','0');
+//     $('.SkillsPhoto img:nth-child(2)').css('opacity','1');
+// }
+//
+// function skillsPhotoBlinkFadeOut() {
+//     $('.SkillsPhoto').css('transition','1s');
+//     $('.SkillsPhoto img:first-child').css('opacity','1');
+//     $('.SkillsPhoto img:nth-child(2)').css('opacity','0');
+// }
+
