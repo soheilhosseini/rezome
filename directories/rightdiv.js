616a521389400f91a3a61764af03b25ceaf00f10
soheilhosseini <hosseini.soheil@yahoo.com>
2017-12-26 17:34:55 +0330

    rezome taghier kard

--- directories/rightdiv.js
+++ directories/rightdiv.js
@@ -3,44 +3,55 @@ app.directive("rightDiv", function () {
         templateUrl: 'directories/rightdiv.html',
         controller: function () {
 
-            // blink();
-            //
-            // window.setInterval(blink, 8000);
-            // function blink() {
-            //
-            //     setTimeout(fadeOut,0);
-            //     setTimeout(appear,6000);
-            //     function fadeOut() {
-            //         for (var i = 0; i < 132; i++) {
-            //             $('#mainRightDiv i:eq(' + i + ')').css({color: '#322d38'});
-            //         }
-            //     }
-            //     function appear() {
-            //         for (var i = 1; i < 10; i++) {
-            //             var ran = Math.floor(Math.random() * 132) + 1;
-            //             $('#mainRightDiv i:eq(' + ran + ')').css({color: '#4a4453'});
-            //             $('#mainRightDiv i:eq(' + ran + ')').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)');
-            //         }
-            //     }
-            //
-            //
-            // }
-            // window.setInterval(fadeOutShadow, 40000);
-            // function fadeOutShadow() {
-            //     for (var i = 0; i < 132; i++) {
-            //         // var ran = Math.floor(Math.random() * 140) + 1;
-            //         $('#mainRightDiv i:eq(' + i + ')').css('text-shadow', '0px 0px 0px rgba(31, 30, 31, 0.76)');
-            //
-            //     }
-            // }
-            //
-            // $(document).ready(function () {
-            //     $("#mainRightDiv i").hover(function () {
-            //         $(this).css('color', 'white');
-            //         $(this).css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
-            //
-            //     });
-            // });
+            blink();
+
+            window.setInterval(blink, 8000);
+            function blink() {
+
+                setTimeout(fadeOut,0);
+                setTimeout(appear,6000);
+                function fadeOut() {
+                    for (var i = 0; i < 170; i++) {
+                        $('#mainRightDiv i:eq(' + i + ')').css({color: '#1d233a'});
+                        $('#mainLeftDiv i:eq(' + i + ')').css({color: '#1d233a'});
+
+                    }
+                }
+                function appear() {
+                    for (var i = 1; i < 3; i++) {
+                        var ran = Math.floor(Math.random() * 170) + 1;
+                        $('#mainRightDiv i:eq(' + ran + ')').css({color: '#959595'});
+                        $('#mainRightDiv i:eq(' + ran + ')').css('text-shadow', '2px 1px 8px black');
+
+                        $('#mainLeftDiv i:eq(' + ran + ')').css({color: '#959595'});
+                        $('#mainLeftDiv i:eq(' + ran + ')').css('text-shadow', '2px 1px 8px black');
+                    }
+                }
+
+
+            }
+            window.setInterval(fadeOutShadow, 40000);
+            function fadeOutShadow() {
+                for (var i = 0; i < 170; i++) {
+                    // var ran = Math.floor(Math.random() * 140) + 1;
+                    $('#mainRightDiv i:eq(' + i + ')').css('text-shadow', '0px 0px 0px rgba(31, 30, 31, 0.76)');
+
+                    $('#mainLeftDiv i:eq(' + i + ')').css('text-shadow', '0px 0px 0px rgba(31, 30, 31, 0.76)');
+
+
+                }
+            }
+
+            $(document).ready(function () {
+                $("#mainRightDiv i").hover(function () {
+                    $(this).css('color', 'white');
+                    $(this).css('text-shadow', '2px 1px 8px black')
+                });
+                $("#mainLeftDiv i").hover(function () {
+                    $(this).css('color', 'white');
+                    $(this).css('text-shadow', '2px 1px 8px black')
+                });
+            });
         }
     }
 });
--- indexJs.js
+++ indexJs.js
@@ -19,46 +19,46 @@
 //     $('.SkillsPhoto img:nth-child(2)').css('opacity','0');
 // }
 
-$('.skillsHover').hover(function () {
-    if ($(this).find('div:eq(0)').html() == 'HTML') {
-        $('.icon-HTML5').css('color', 'white');
-        $('.icon-HTML5').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
-    }
-    if ($(this).find('div:eq(0)').html() == 'CSS') {
-        $('.icon-CSS3').css('color', 'white');
-        $('.icon-CSS3').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
-    }
-    if ($(this).find('div:eq(0)').html() == 'Javascript') {
-        $('.icon-javascript').css('color', 'white');
-        $('.icon-javascript').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
-    }
-    if ($(this).find('div:eq(0)').html() == 'Jquery') {
-        $('.icon-Jquery').css('color', 'white');
-        $('.icon-Jquery').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
-    }
-    if ($(this).find('div:eq(0)').html() == 'Bootstrap') {
-        $('.icon-Boostrap').css('color', 'white');
-        $('.icon-Boostrap').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
-    }
-    if ($(this).find('div:eq(0)').html() == 'Materialize') {
-        $('.icon-materiaze').css('color', 'white');
-        $('.icon-materiaze').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
-    }
-    if ($(this).find('div:eq(0)').html() == 'AngularJs') {
-        $('.icon-ANgualrJS').css('color', 'white');
-        $('.icon-ANgualrJS').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
-    }
-    if ($(this).find('div:eq(0)').html() == 'Sass') {
-        $('.icon-sass-1').css('color', 'white');
-        $('.icon-sass-1').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)');
-    }
-    if ($(this).find('div:eq(0)').html() == 'Github') {
-        $('.icon-github').css('color', 'white');
-        $('.icon-github').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
-    }
-    if ($(this).find('div:eq(0)').html() == 'Photoshop') {
-        $('.icon-photoshop').css('color', 'white');
-        $('.icon-photoshop').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
-    }
-})
+// $('.skillsHover').hover(function () {
+//     if ($(this).find('div:eq(0)').html() == 'HTML') {
+//         $('.icon-HTML5').css('color', 'white');
+//         $('.icon-HTML5').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
+//     }
+//     if ($(this).find('div:eq(0)').html() == 'CSS') {
+//         $('.icon-CSS3').css('color', 'white');
+//         $('.icon-CSS3').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
+//     }
+//     if ($(this).find('div:eq(0)').html() == 'Javascript') {
+//         $('.icon-javascript').css('color', 'white');
+//         $('.icon-javascript').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
+//     }
+//     if ($(this).find('div:eq(0)').html() == 'Jquery') {
+//         $('.icon-Jquery').css('color', 'white');
+//         $('.icon-Jquery').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
+//     }
+//     if ($(this).find('div:eq(0)').html() == 'Bootstrap') {
+//         $('.icon-Boostrap').css('color', 'white');
+//         $('.icon-Boostrap').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
+//     }
+//     if ($(this).find('div:eq(0)').html() == 'Materialize') {
+//         $('.icon-materiaze').css('color', 'white');
+//         $('.icon-materiaze').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
+//     }
+//     if ($(this).find('div:eq(0)').html() == 'AngularJs') {
+//         $('.icon-ANgualrJS').css('color', 'white');
+//         $('.icon-ANgualrJS').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
+//     }
+//     if ($(this).find('div:eq(0)').html() == 'Sass') {
+//         $('.icon-sass-1').css('color', 'white');
+//         $('.icon-sass-1').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)');
+//     }
+//     if ($(this).find('div:eq(0)').html() == 'Github') {
+//         $('.icon-github').css('color', 'white');
+//         $('.icon-github').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
+//     }
+//     if ($(this).find('div:eq(0)').html() == 'Photoshop') {
+//         $('.icon-photoshop').css('color', 'white');
+//         $('.icon-photoshop').css('text-shadow', '2px 1px 3px rgba(31, 30, 31, 0.76)')
+//     }
+// })
 
